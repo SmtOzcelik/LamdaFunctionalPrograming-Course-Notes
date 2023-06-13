ec40f45e0d7abbf3ae009c6030a24608a7ca1505
samet ozcelik <smtab142790@gmail.com>
2023-06-13 21:29:42 +0300

    13/06/2023

--- src/lamda_functional_programing01/Fp02.java
+++ src/lamda_functional_programing01/Fp02.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Fp02 {
     public static void main(String[] args) {
+
         //2) Cift list elemanlerini ayni satirda aralarinda bosluk bırakarak yazdiran bir metohod olusturun(Structured)
 
         ArrayList<Integer>list=new ArrayList<>(Arrays.asList(8,9,131,10,9,10,2,8));

77cd77ab8aac7b843550f5a65ee9d5639513af5a
samet ozcelik <smtab142790@gmail.com>
2023-06-16 11:02:53 +0300

    16/06/2023

---
+++ src/lamda_functional_programing01/Fp05.java
@@ -0,0 +1,21 @@
+package lamda_functional_programing01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Fp05 {
+    public static void main(String[] args) {
+        //5) Tekrarsiz cift elemanlarin kareleri toplamini hesaplayan bir method olusturun
+
+        ArrayList<Integer> list=new ArrayList<>(Arrays.asList(2,9,4,2,11,9,4,7,11));
+
+        tekrarsizCiftElemanlarinKareToplami(list); //20
+
+    }
+    public static void tekrarsizCiftElemanlarinKareToplami(List<Integer>list){
+
+        Integer toplam=list.stream().distinct().filter(t->t%2==0).map(t->t*t).reduce(0,(t,u)->t+u);
+        System.out.println(toplam);
+    }
+}

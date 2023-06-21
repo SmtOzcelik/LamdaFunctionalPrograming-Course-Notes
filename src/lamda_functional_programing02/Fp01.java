ad39bd8c33348f5d3dcdc6a9ca26530c7a40b70c
samet ozcelik <smtab142790@gmail.com>
2023-06-21 11:22:06 +0300

    21/06/2023

---
+++ src/lamda_functional_programing02/Fp01.java
@@ -0,0 +1,33 @@
+package lamda_functional_programing02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Fp01 {
+    /*
+    1- t -> "Logic" , (t,u) -> "Logic"
+       Bu yapiya Lambda Expession
+    2- Functional Programming kapsaminda Lambda Expession kullanilabilir ama onerilmez.
+       Lambda Expession yerine Method Reference tercih edilir.
+    3- Method Reference kullanimi "Class Name :: Method Name"
+
+       Ayni zamanda kendi class larinizi da kullanabilirsiniz
+       Ornegin bir Animal class imiz var ve bu class "eat()" methoduna sahip ==> "Animal :: eat"
+     */
+
+
+
+    public static void main(String[] args) {
+        // 1) Ardisik list elementlerini ayni satirda aralarinda bosluk bırakarak
+        //    yazdiran bir method olusturun(Functional ve method reference)
+
+        ArrayList<Integer>list=new ArrayList<>(Arrays.asList(8,9,131,10,9,10,2,8));
+
+        listElemanlariniYazdirFunctional(list);
+    }
+    public static void listElemanlariniYazdirFunctional(List<Integer>list){
+
+        list.stream().forEach(Utils::ayniSatirdaBosluklaYazdir);
+    }
+}

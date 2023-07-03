c913b47827795d054b158512ba7d096f1f7d2752
samet ozcelik <smtab142790@gmail.com>
2023-07-03 22:47:00 +0300

    02/07/2023

--- src/lamda_functional_programing02/Fp09.java
+++ src/lamda_functional_programing02/Fp09.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Fp09 {
     public static void main(String[] args) {
+
         //9) List elemanlari arasindan 7 den buyuk cift , en kucuk degeri bulan bir method olusturun
 
         ArrayList<Integer> list=new ArrayList<>(Arrays.asList(8,9,131,10,9,10,2,8));

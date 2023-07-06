cf9c2750231c9861d9d5613e221705493159af81
samet ozcelik <smtab142790@gmail.com>
2023-07-07 00:34:59 +0300

    06/07/2023

---
+++ src/lamda_functional_programing03/Fp03.java
@@ -0,0 +1,26 @@
+package lamda_functional_programing03;
+
+import lamda_functional_programing02.Utils;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Fp03 {
+    public static void main(String[] args) {
+    List<String> list=new ArrayList<>();
+    list.add("Ali"); list.add("Ali"); list.add("Mark"); list.add("Amanda");
+    list.add("Christopher"); list.add("Jackson"); list.add("Mariano");
+    list.add("Alberto"); list.add("Tucker");  list.add("Benjamin");
+
+    System.out.println(list);
+    uzunlugaGoreTersYazdir(list);
+
+
+}
+// 3-) Elemanları uzunluklarına göre ters sıralayıp yazdıran bir method oluşturun.
+
+    public static void uzunlugaGoreTersYazdir(List<String>list){
+    list.stream().sorted(Comparator.comparing(String::length).reversed()).forEach(Utils::ayniSatirdaBosluklaYazdir);
+    }
+}

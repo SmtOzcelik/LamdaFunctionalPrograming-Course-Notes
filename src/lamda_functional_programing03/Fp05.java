49d8e4615300198cb3da9dcc301fbc0bd7c0ae89
samet ozcelik <smtab142790@gmail.com>
2023-07-09 13:38:01 +0300

    08/07/2023

---
+++ src/lamda_functional_programing03/Fp05.java
@@ -0,0 +1,29 @@
+package lamda_functional_programing03;
+
+import lamda_functional_programing02.Utils;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Fp05 {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("Ali"); list.add("Ali"); list.add("Mark"); list.add("Amanda");
+        list.add("Christopher"); list.add("Jackson"); list.add("Mariano");
+        list.add("Alberto"); list.add("Tucker");  list.add("Benjamin");
+
+        System.out.println(list);
+        uzunlukVeIlkHarfeGoreSiralaYazdir(list);
+
+    }
+    /* 5) Elemanları önce uzunluklarına göre ve sonra ilk karakterine göre sıralayıp
+    yazdıran bir method oluşturun.
+     */
+    public static void uzunlukVeIlkHarfeGoreSiralaYazdir(List<String> list){
+
+        list.stream().sorted(Comparator.comparing(String::length).
+                thenComparing(Utils::ilkKarakteriniVer)).
+                forEach(Utils::ayniSatirdaBosluklaYazdir);
+    }
+}

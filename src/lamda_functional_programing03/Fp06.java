c72616fbcae409cba8f33939a316dbb0fb203f68
samet ozcelik <smtab142790@gmail.com>
2023-07-10 22:52:44 +0300

    09/07/2023

---
+++ src/lamda_functional_programing03/Fp06.java
@@ -0,0 +1,25 @@
+package lamda_functional_programing03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fp06 {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("Ali"); list.add("Ali"); list.add("Mark"); list.add("Amanda");
+        list.add("Christopher"); list.add("Jackson"); list.add("Mariano");
+        list.add("Alberto"); list.add("Tucker");  list.add("Benjamin");
+
+        System.out.println(list);
+        bestenBuyukleriSil(list);
+
+    }
+    //6-) Uzunluğu 5'ten büyük olan elemanları silen bir method oluşturun.
+
+    public static void bestenBuyukleriSil(List<String> list){
+
+        list.removeIf(t->t.length()>5);
+        System.out.println(list);
+
+    }
+}

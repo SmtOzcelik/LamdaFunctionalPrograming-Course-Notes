3778ce3dbb295c7ece6ddb97b596454a43aaafca
samet ozcelik <smtab142790@gmail.com>
2023-07-11 10:34:36 +0300

    10/07/2023

---
+++ src/lamda_functional_programing03/Fp07.java
@@ -0,0 +1,24 @@
+package lamda_functional_programing03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fp07 {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("Ali"); list.add("Ali"); list.add("Mark"); list.add("Amanda");
+        list.add("Christopher"); list.add("Jackson"); list.add("Mariano");
+        list.add("Alberto"); list.add("Tucker");  list.add("Benjamin");
+
+        System.out.println(list);
+        baslagiciAYadaSonuNOlaniSil01(list);
+
+    }
+    //7-) ‘A’, ‘a’ ile başlayan yada ‘N’, ‘n’ ile biten elemanları silen bir method oluşturun.
+
+    public static void baslagiciAYadaSonuNOlaniSil01(List<String> list){
+
+        list.removeIf(t->t.startsWith("A") || t.startsWith("a") || t.endsWith("N") || t.endsWith("n"));
+        System.out.println(list);
+    }
+}
